e08eb490d36c489103908976f9fe7b23932ba534
Darius Roberts <darius.roberts@gmail.com>
2013-09-12 17:28:09 -0400

    recursively create vimrc dirs if not present

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,27 @@
 #
 #   include vundle
 class vundle {
-  $vundle_dir = "${boxen::config::homedir}/.vimrc/bundle/vundle"
-  file { $vundle_dir:
-    ensure   => 'directory'
+  $vundle_dir = "${boxen::config::homedir}/.vim/bundle/vundle"
+  #file { $vundle_dir:
+  #  ensure   => 'directory'
+  #}
+  exec { 'mkdir_vundle_dir':
+      path    => [ '/bin', '/usr/bin' ],
+      command => "mkdir -p ${vundle_dir}"
   }
   repository { $vundle_dir:
     source   => 'https://github.com/gmarik/vundle.git',
   }
 
+
   #Launch vim, run :BundleInstall (or vim +BundleInstall +qall for CLI lovers)
+ #vim -c ":BundleInstall"
+  exec { "install via 'BundleInstall'":
+    cwd      => $vundle_dir,
+    #path    => [ '/bin', '/usr/bin' ],
+    command  => "vim +BundleInstall +qall foo.txt",
+    provider => shell
+    #creates  => "${home}/.zshrc",
+    #require  => Repository[$dotfiles_dir]
+  }
 }
